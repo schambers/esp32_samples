e26483df8b18c6067c6f40d7753767f0eb839081
schambers <schambers80@gmail.com>
2021-09-04 15:02:15 -0500

    board loayout for HiLetgo ESP32, adding two sample sketches

--- sketch_blink-led/sketch_blink-led.ino
+++ sketch_blink-led/sketch_blink-led.ino
@@ -9,13 +9,9 @@
 #define LED 2
 
 void setup() {
-  // Set pin mode
-  pinMode(LED,OUTPUT);
+
 }
 
 void loop() {
-  delay(500);
-  digitalWrite(LED,HIGH);
-  delay(500);
-  digitalWrite(LED,LOW);
+
 }
